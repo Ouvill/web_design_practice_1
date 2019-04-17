0688aec04ddf872f673774ea9620aa7c67e8e01a
Ouvill <contact@ouvill.net>
2019-04-17 16:24:30 +0900

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,41 @@ import "./App.scss";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="Site">
+        <header className="Site-header">
+          <div className="top-wrapper">
+            <div className="top-container">
+              <div className="top-image" />
+              <h1>サイトデザイン練習</h1>
+              <h2>第一弾 : トップ画像を用意する</h2>
+              <p>
+                プログラムを作れるだけで、見た目が全然かっこよくなければ利用する気がおきないと思うので、デザインをちょっと頑張る
+              </p>
+
+              <button
+                className="btn"
+                onClick={() => {
+                  alert("ありがとうございます");
+                }}
+              >
+                ガンバレ！！
+              </button>
+            </div>
+          </div>
         </header>
+        <main>
+          <div className="contant">
+            <h2>使用画像</h2>
+            <p>
+              画像を利用するにあたっては{" "}
+              <a href="https://unsplash.com/">unsplash</a> の{" "}
+              <a href="https://unsplash.com/photos/f77Bh3inUpE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                Arnold Francisca の 画像
+              </a>
+              を利用しました。
+            </p>
+          </div>
+        </main>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "normalize.css";
-import Site from "./Site";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Site />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
