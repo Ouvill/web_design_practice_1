ae5f6cdeb48c5b32528d68800e6af9c466f76d67
Ouvill <contact@ouvill.net>
2019-04-17 15:03:15 +0900

    import normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "normalize.css";
 import Site from "./Site";
 import * as serviceWorker from "./serviceWorker";
 
